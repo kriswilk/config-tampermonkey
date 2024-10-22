7fde3fd207d83386c2f1d8e47adfd00b932b31d6
Kris Wilk <kris@reefnet.ca>
2024-10-22 13:03:18 -0400

    Update cosmetic_filter_ebay.js

--- cosmetic_filter_ebay.js
+++ cosmetic_filter_ebay.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Cosmetic Filter - eBay
 // @namespace    http://tampermonkey.net/
-// @version      2
-// @description  
+// @version      3
+// @description  Hide visual distractions such as similar / sponsored items, suggestions, etc.
 // @author       Kris Wilk
 // @match        https://www.ebay.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ebay.com
